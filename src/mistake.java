ebf7634aa8eca8267b63d7cbeaca60801d2b864c
Cole <league@WTS5.attlocal.net>
2016-10-24 20:01:12 -0700

    ahhhh

---
+++ src/mistake.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class mistake {
+	public static void main(String[] args) {
+		for (int i = 0; i < 120; i++) {
+			JOptionPane.showMessageDialog(null, "jajajajaaaaaaaa");
+		
+		}
+	}
+
+}
